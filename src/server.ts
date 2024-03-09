142c7c39fe436b09cfe91501e64b929c76a8b9a2
mohin <mohinsheikh2011@gmail.com>
2024-03-09 16:52:52 +0530

    added express-fileupload

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,13 @@ import {
   notFoundMiddleware,
   errorHandlerMiddleware,
 } from "./middlewares/errorHandlers.middleware";
-
+import fileUpload from 'express-fileupload';
 import deserializeUser from "./middlewares/deserializeUser.middleware";
 
 function createServer() {
   const app = express();
 
+  app.use(fileUpload());
   app.use(express.json());
   app.use(deserializeUser);
   app.use(loggerMiddleware);
